96ab78e9e701991816b2b84d0ac6f00e3cd2c231
Slobodov Aleksandr <gerons.y@gmail.com>
2017-11-24 20:06:20 +0000

    Test GUI is now more like actual GUI.

--- src/Artatawe/GUI/Test.java
+++ src/Artatawe/GUI/Test.java
@@ -1,33 +1,65 @@
 package Artatawe.GUI;
 
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import static javafx.scene.input.MouseEvent.MOUSE_PRESSED;
 
-public class Test extends Application{
+public class Test extends Application {
+
+
+    JFXHamburger menuHamburger = new JFXHamburger();
+    Label nameField = new Label("ARTATAWE");
 
     public static void main(String[] args) {
-
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws IOException{
-        FXMLLoader loader = new FXMLLoader();
-        String fxmlFocPath = "src/Artatawe/GUI/Test.fxml";
-        FileInputStream fxmlStream = new FileInputStream(fxmlFocPath);
-        Pane root = (Pane) loader.load(fxmlStream);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Hello World");
-        stage.show();
+    public void start(Stage primaryStage) {
+        BorderPane border = new BorderPane();
+        StackPane stack = new StackPane();
+        HBox hbox = addHBox();
+        border.setTop(hbox);
+        primaryStage.setTitle("Artatawe");
+
+
+        Scene scene = new Scene(border, 600, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        initialize();
 
     }
 
+    public HBox addHBox() {
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(15, 12, 15, -25));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+
+        menuHamburger.setPrefSize(100, 20);
+        nameField.setStyle("-fx-font-size: 30; -fx-color-label-visible: false; -fx-text-fill: #FFFFFF");
+
+        hbox.getChildren().addAll(menuHamburger,nameField);
+
+        return hbox;
+    }
+
+    public void initialize() {
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(menuHamburger);
+        transition.setRate(-1);
+        menuHamburger.addEventHandler(MOUSE_PRESSED,(e)->{
+            transition.setRate(transition.getRate()*-1);
+            transition.play();
+        });
+    }
 }
