87e5813059b6d75d277c2d00219a177ec3092566
charliedaley97 <33719163+charliedaley97@users.noreply.github.com>
2017-11-25 22:37:51 +0000

    Update Painting.java

--- src/Artatawe/Data/Painting.java
+++ src/Artatawe/Data/Painting.java
@@ -1,5 +1,16 @@
 package Artatawe.Data;
 
-public class Painting {
-	
+import java.util.Date;
+public class Painting extends Artwork{
+
+    public Painting(String name, String description, Image photo, int year, int reservedPrice, int allowedBids, Date dateTime, int width, int height){
+        super(name, description, photo, year, reservedPrice, allowedBids, dateTime, width, height);
+
+
+    }
+
+    /*public String toString(){
+        return (getName()+" "+ getDescription() +" "+ getPhoto() +" " + getYear() +" " + getReservedPrice()
+                +" " + getAllowedBids() +" " + getDateTime() +" " + getWidth() +" " + getHeight());
+    }*/
 }
