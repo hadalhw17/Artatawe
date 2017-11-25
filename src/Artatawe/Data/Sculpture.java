72adcee6c92d2ba6d83626cc02136ddcfd12746f
charliedaley97 <33719163+charliedaley97@users.noreply.github.com>
2017-11-25 22:38:20 +0000

    Update Sculpture.java

--- src/Artatawe/Data/Sculpture.java
+++ src/Artatawe/Data/Sculpture.java
@@ -1,5 +1,27 @@
 package Artatawe.Data;
 
-public class Sculpture {
-	
+import java.util.Date;
+
+public class Sculpture extends Artwork {
+
+    private int depth;
+
+
+
+    public Sculpture(String name, String description, Image photo, int year, int reservedPrice, int allowedBids, Date dateTime, int width, int height, int depth){
+        super(name, description, photo, year, reservedPrice, allowedBids, dateTime, width, height);
+
+        this.depth = depth;
+
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    /*public String toString(){
+        return (getName()+" "+ getDescription() +" "+ getPhoto() +" " + getYear() +" " + getReservedPrice()
+                +" " + getAllowedBids() +" " + getDateTime() +" " + getWidth() +" " + getHeight() +" " + getDepth());
+    }*/
+
 }
