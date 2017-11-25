e08ddfe31cfb6c0179bd5fffdbabfb8f908bbc7b
hadalhw17 <lubluShpak228>
2017-11-25 11:14:26 +0000

    Test.java replaced with GUIConroller.java Added ProfileScene.java which is a sub class for ScenePattern.java

---
+++ src/Artatawe/GUI/GUIController.java
@@ -0,0 +1,29 @@
+package Artatawe.GUI;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUIController extends Application {
+
+
+
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Artatawe");
+
+        Scene profileScene = new Scene(new ProfileScene().getPane(), 600, 600);
+        primaryStage.setScene(profileScene);
+        primaryStage.show();
+
+    }
+
+
+}
