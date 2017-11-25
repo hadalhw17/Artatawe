67009c20309bb61eb43a2c8a139745c5aa0a47d6
hadalhw17 <lubluShpak228>
2017-11-25 10:47:55 +0000

    Sliding menu added

--- src/Artatawe/GUI/Test.java
+++ src/Artatawe/GUI/Test.java
@@ -1,5 +1,7 @@
 package Artatawe.GUI;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.application.Application;
@@ -9,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import static javafx.scene.input.MouseEvent.MOUSE_PRESSED;
@@ -17,7 +20,10 @@ public class Test extends Application {
 
 
     JFXHamburger menuHamburger = new JFXHamburger();
-    Label nameField = new Label("ARTATAWE");
+    Label nameField = new Label("ARTATAWE | ");
+    JFXDrawer leftDrawer = new JFXDrawer();
+    VBox drawePane = new VBox();
+
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -29,13 +35,37 @@ public class Test extends Application {
         StackPane stack = new StackPane();
         HBox hbox = addHBox();
         border.setTop(hbox);
-        primaryStage.setTitle("Artatawe");
+        drawePane = addVBox();
 
+        leftDrawer.setDefaultDrawerSize(300);
+        leftDrawer.setOverLayVisible(false);
+        leftDrawer.setSidePane(drawePane);
+        JFXButton button = new JFXButton("Hello");
+
+        border.setLeft(leftDrawer);
+        primaryStage.setTitle("Artatawe");
 
         Scene scene = new Scene(border, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
         initialize();
+
+    }
+
+    public VBox addVBox() {
+        JFXButton button1 = new JFXButton("View Auctions");
+        JFXButton button2 = new JFXButton("Bid artwork");
+        VBox vBox = new VBox();
+        vBox.setStyle("-fx-background-color: #E91E63; -fx-padding: -10;");
+        vBox.setPadding(new Insets(15, 12, 15, 0));
+        vBox.setSpacing(10);
+        vBox.setMargin(button1,new Insets(25,25,1,25));
+        button1.setMaxWidth(10000);
+        vBox.setMargin(button2,new Insets(1,25,25,25));
+        button2.setMaxWidth(10000);
+        vBox.getChildren().addAll(button1,button2);
+        return vBox;
+
 
     }
 
@@ -56,10 +86,17 @@ public class Test extends Application {
 
     public void initialize() {
         HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(menuHamburger);
+        drawePane = addVBox();
         transition.setRate(-1);
         menuHamburger.addEventHandler(MOUSE_PRESSED,(e)->{
             transition.setRate(transition.getRate()*-1);
             transition.play();
+
+            if (leftDrawer.isShown()){
+                leftDrawer.close();
+            } else{
+                leftDrawer.open();
+            }
         });
     }
 }
