91472821bc95fbf358ffe449e81eb31caeab77fe
hadalhw17 <lubluShpak228>
2017-11-26 00:44:34 +0000

    Profile's added artworks card added.

--- src/Artatawe/GUI/ProfileScene.java
+++ src/Artatawe/GUI/ProfileScene.java
@@ -2,10 +2,12 @@ package Artatawe.GUI;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXMasonryPane;
+import com.jfoenix.controls.JFXTextArea;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.io.File;
@@ -34,14 +36,35 @@ public class ProfileScene extends ScenePattern{
         if(imgView == null){
             imgView = new ImageView(new Image(getClass().getResource("/img1.png").toExternalForm()));
         }
-
         imgView.setFitWidth(200);
         imgView.setFitHeight(200);
         AnchorPane pane = new AnchorPane();
         pane.getChildren().add(imgView);
-        VBox pane1 = new VBox(new Label("That's insane dude"), new JFXButton("Press Me"));
-        pane1.setStyle("-fx-background-color: red");
-        contentPane.getChildren().addAll(pane,pane1);
+        JFXMasonryPane pane2 = new JFXMasonryPane();
+        ImageView imgView2 = new ImageView(new Image(getClass().getResource("/img2.png").toExternalForm()));
+        ImageView imgView3 = new ImageView(new Image(getClass().getResource("/img1.png").toExternalForm()));
+        imgView2.setImage(new Image(getClass().getResource("/img2.png").toExternalForm()));
+        imgView3.setImage(new Image(getClass().getResource("/img1.png").toExternalForm()));
+        ImageView imgView4 = new ImageView(new Image(getClass().getResource("/img2.png").toExternalForm()));
+        ImageView imgView5 = new ImageView(new Image(getClass().getResource("/img1.png").toExternalForm()));
+        imgView2.setImage(new Image(getClass().getResource("/img2.png").toExternalForm()));
+        imgView3.setImage(new Image(getClass().getResource("/img1.png").toExternalForm()));
+        pane2.getChildren().addAll(new Pane(imgView2),new Pane(imgView3),new Pane(imgView4),new Pane(imgView5));
+        pane2.setPrefSize(120,150);
+        pane2.setHSpacing(-1);
+        //pane2.setVSpacing(1);
+        imgView2.setFitWidth(50);
+        imgView2.setFitHeight(50);
+        imgView3.setFitWidth(50);
+        imgView3.setFitHeight(50);
+        imgView4.setFitWidth(50);
+        imgView4.setFitHeight(50);
+        imgView5.setFitWidth(50);
+        imgView5.setFitHeight(50);
+        VBox pane1 = new VBox(new Label("About"), new Label("Name: Aleksandr Slobodov\nAge:20y.o."));
+        pane1.setStyle("-fx-effect: dropshadow(gaussian, silver, 10, 0, 0, 0); -fx-background-color: #E8EAF6;");
+        pane2.setStyle("-fx-effect: dropshadow(gaussian, silver, 10, 0, 0, 0);-fx-background-color: #E8EAF6;");
+        contentPane.getChildren().addAll(pane,pane1,pane2);
 
 
 
