0619c4a7d7ec8c47acf49c7525aaada127bf4bc0
hadalhw17 <lubluShpak228>
2017-11-26 12:52:43 +0000

    Code is more readable now. Added comments. Optimised imports.

--- src/Artatawe/GUI/ScenePattern.java
+++ src/Artatawe/GUI/ScenePattern.java
@@ -10,47 +10,84 @@ import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.io.File;
-
 import static javafx.scene.input.MouseEvent.MOUSE_CLICKED;
 import static javafx.scene.input.MouseEvent.MOUSE_PRESSED;
 
+
+/**
+ *  @author Aleksandr Slobodov
+ *
+ *  ScenePattern.java
+ *
+ *  A pattern for all of GUI windows.
+ *  Contains BorderPane as main layout, VBox as left menu, HBox as a header.
+ *
+ */
 public class ScenePattern{
-    private JFXHamburger menuHamburger = new JFXHamburger();
-    private Label nameField = new Label("ARTATAWE | ");
-    private JFXDrawer leftDrawer = new JFXDrawer();
-    private VBox drawerPane = new VBox();
-    private BorderPane border = new BorderPane();
+
+    //Name printed in top left corner
+    private final Label NAME_FIELD = new Label("ARTATAWE | ");
+
+    //Animated button for menu sliding
+    private JFXHamburger menuHamburger;
+    //Left menu
+    private JFXDrawer leftDrawer;
+    //Pane for left menu
+    private VBox drawerPane;
+    //Main pane of scene
+    private BorderPane border;
+    //Central content pane
     private JFXMasonryPane contentPane;
-    private Label nameLabel = new Label("Welcome");
+    //Label for name of the current page
+    private Label nameLabel;
+    //Header panel
+    private HBox topBox;
 
 
+    /**
+     * A constructor for ScenePattern.
+     * Adds all of the main elements to the scene.
+     */
     public ScenePattern() {
-        HBox hbox = addHBox();
-        border.setTop(hbox);
+        nameLabel = new Label();
+        border = new BorderPane();
+        leftDrawer = new JFXDrawer();
+        drawerPane = new VBox();
+        border = new BorderPane();
+        contentPane = new JFXMasonryPane();
+        nameLabel = new Label();
+        topBox = addHBox();
+        border.setTop(topBox);
         drawerPane = addDrawerVBox();
-        leftDrawer.setDefaultDrawerSize(300);
-        leftDrawer.setOverLayVisible(false);
-        leftDrawer.setSidePane(drawerPane);
-        leftDrawer.setPrefSize(0,0);
         border.setLeft(leftDrawer);
         contentPane = constructContentPane();
         border.setCenter(contentPane);
-        initialize();
+        menuActivity();
     }
 
+    /**
+     * A getter for a main pane.
+     * @return main border pane with all of the objects constructed on it.
+     */
     public BorderPane getPane() {
         return border;
     }
 
+    /**
+     * Generates VBox with a drawer menu in it.
+     * @return generated VBox
+     */
     public VBox addDrawerVBox() {
+        leftDrawer.setDefaultDrawerSize(300);
+        leftDrawer.setOverLayVisible(false);
+        leftDrawer.setSidePane(drawerPane);
+        leftDrawer.setPrefSize(0,0);
         JFXButton profileButton = new JFXButton("View Profile");
         JFXButton button1 = new JFXButton("View Auctions");
         JFXButton button2 = new JFXButton("Bid artwork");
@@ -76,33 +113,48 @@ public class ScenePattern{
         return vBox;
     }
 
+    /**
+     * Generates HBox for a header
+     * @return generated header.
+     */
     public HBox addHBox() {
         HBox hbox = new HBox();
+        menuHamburger = new JFXHamburger();
         hbox.setPadding(new Insets(15, 12, 15, -25));
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color: #336699;");
-
-
         menuHamburger.setPrefSize(100, 20);
-        nameField.setStyle("-fx-font-size: 30; -fx-color-label-visible: false; -fx-text-fill: #FFFFFF");
+        NAME_FIELD.setStyle("-fx-font-size: 30; -fx-color-label-visible: false; -fx-text-fill: #FFFFFF");
         nameLabel.setStyle("-fx-font-size: 30; -fx-color-label-visible: false; -fx-text-fill: #FFFFFF");
 
-        hbox.getChildren().addAll(menuHamburger, nameField,nameLabel);
+        hbox.getChildren().addAll(menuHamburger, NAME_FIELD,nameLabel);
 
         return hbox;
     }
 
+    /**
+     * Sets text for page name label
+     * @param nameOfPage name to add
+     */
     public void setNameLabel(String nameOfPage){
-        this.nameLabel.setText(nameOfPage);
+        nameLabel.setText(nameOfPage);
     }
 
+    /**
+     * Generates central content pane.
+     * @return generated content pane.
+     */
     public JFXMasonryPane constructContentPane(){
 
         return contentPane;
     }
 
 
-    public void initialize() {
+    /**
+     * Responsible for hamburger button events.
+     * Also responsible for side menu animations
+     */
+    public void menuActivity() {
         HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(menuHamburger);
         drawerPane = addDrawerVBox();
         transition.setRate(-1);
