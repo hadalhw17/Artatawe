64e291d48dbf8951dd0fc9fd9df9dbc919ee15cd
Alexis Venizelos <alexis.venizelos@gmail.com>
2017-11-27 16:49:17 +0000

    Add files via upload

--- src/Artatawe/Data/Bid.java
+++ src/Artatawe/Data/Bid.java
@@ -1,5 +1,54 @@
 package Artatawe.Data;
-
+import java.util.Date;
+/**
+ * @author alexisvenizelos
+ *
+ */
 public class Bid {
 	
+	private Profile buyer;
+	private int amount;
+	private Date dateTime;
+	private int auction;
+	
+	public Bid(Profile buyer, int amount, Date dateTime, int auction) {
+		
+		this.buyer = buyer;
+		this.amount = amount;
+		this.dateTime = dateTime;
+		this.auction = auction;
+		
+	}
+
+	public int getAuction() {
+		return auction;
+	}
+
+	public void setAuction(int auction) {
+		this.auction = auction;
+	}
+
+	public String getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(String buyer) {
+		this.buyer = buyer;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
 }
