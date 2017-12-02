c26571ae2d32603238942078493657c7d1630d03
Schlongberg <33962163+Schlongberg@users.noreply.github.com>
2017-12-02 04:27:47 +0000

    Add files via upload

--- src/Artatawe/Data/Address.java
+++ src/Artatawe/Data/Address.java
@@ -1,5 +1,68 @@
-package Artatawe.Data;
+import java.util.ArrayList;
 
 public class Address {
+	 
+	private int houseNum;
+	private String street; 
+	private String county;
+	private String postcode; 
+	private String city;
+
+	
+	private ArrayList<Address> address = new ArrayList<Address>();
+	
+	
+	public Address(int houseNum, String street, String city, String county, String postcode){
+		this.houseNum = houseNum;
+		this.street  = street;
+		this.city  = city;
+		this.county = county;
+		this.postcode = postcode;
+}
+	
+	
+	public int getHouseNum() {
+		return houseNum;
+	}
+
+	public void setHouseNum(int houseNum) {
+		this.houseNum = houseNum;
+	}
+
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
 	
 }
--- src/Artatawe/Data/Profile.java
+++ src/Artatawe/Data/Profile.java
@@ -1,5 +1,100 @@
-package Artatawe.Data;
+import java.util.ArrayList;
 
 public class Profile {
+
+	private String username; 
+	private String firstname; 
+	private String surname; 
+	private String mobileNo;
+	private Address address;
+	private Image profileImage ; 
+	
+	
+	private ArrayList<Profile> favourites = new ArrayList<Profile>(); 
+	
+	private ArrayList<Artwork> artwork = new ArrayList<Artwork>(); 
+	
+
+	Profile(String username, String firstName, String surname, String mobileNo, Address address, Image profileImage){
+		
+		this.username = username;
+		this.firstname = firstName;
+		this.surname = surname;
+		this.mobileNo = mobileNo;
+		this.profileImage = profileImage;
+		
+		
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	public void setProfileImg(Image profileImage) {
+		this.profileImage = profileImage;
+	}
+	
+	public Image getProfileImg(Image profileImage) {
+		return profileImage;
+		
+	}
+	
+	public Artwork getArtworks(Artwork artwork) {
+		return artwork;
+	} 
+	
+	public Artwork addArt(Artwork art) {
+		return art;
+	}
+	
+	public Artwork removeArt(Artwork art) {
+		return art;
+	}
+	
+	public Profile addFav(Profile favorites) {
+		return favorites;
+	}
+	
+	public Profile removeFav(Profile favorites) {
+		return favorites;
+	}
+	
+	public String toString(){
+		
+    return // (getName()+" "+ getDescription() +" "+ getPhoto() +" " + getYear() +" " + getReservedPrice()
+            //+" " + getAllowedBids() +" " + getDateTime() +" " + getWidth() +" " + getHeight() +" " + getDepth());
+}
+		
+	}
 	
 }
+
