95f05d74eefe1683942fbdcd0f3275bb9e37d0d0
Slobodov Aleksandr <gerons.y@gmail.com>
2017-12-08 18:02:34 +0000

    Bidding function added. Bids can be placed, but cannpt be saved.

--- src/Artatawe/GUI/ArtworkScene.java
+++ src/Artatawe/GUI/ArtworkScene.java
@@ -1,14 +1,17 @@
 package Artatawe.GUI;
 
 import Artatawe.Data.*;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXMasonryPane;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *  @author Aleksandr Slobodov
@@ -24,6 +27,8 @@ public class ArtworkScene extends ScenePattern {
     //Artwork to be displayed
     private Auction a;
     private Artwork art;
+    Profile p;
+    DataController dc;
 
     /**
      * Constructor for ArtworkScene.java
@@ -32,7 +37,9 @@ public class ArtworkScene extends ScenePattern {
     public ArtworkScene(DataController dc, Profile p, Auction a){
         super(dc,p);
         this.a = a;
+        this.p = p;
         this.art = a.getArtwork();
+        this.dc = dc;
         setNameLabel(art.getName());
         setContentPane();
     }
@@ -46,6 +53,11 @@ public class ArtworkScene extends ScenePattern {
         JFXMasonryPane contentPane = new JFXMasonryPane();
         ImageView imgView = new ImageView();
         ArrayList<String> biddersName = new ArrayList<>();
+        JFXButton bidButton = new JFXButton("Bid this work");
+        bidButton.setOnMousePressed(e->{
+            a.placeBid(p,1,new Date(12,9,1997));
+            dc.save();
+        });
         for(Bid bid:a.getBidList()){
             biddersName.add(bid.getBuyer().getUsername() + "\n");
         }
@@ -56,13 +68,15 @@ public class ArtworkScene extends ScenePattern {
         imgView.setFitWidth(200);
         imgView.setFitHeight(200);
         AnchorPane imgPane = new AnchorPane(imgView);
+        Pane bidPane = new Pane();
+        bidPane.getChildren().add(bidButton);
         VBox aboutCard = new VBox(new Label("ABOUT"), new Label(a.toString()));
         VBox descriptionCard = new VBox(new Label("Description\n"), new Label(art.getDescription()));
         aboutCard.setStyle("-fx-effect: dropshadow(gaussian, silver, 10, 0, 0, 0); -fx-background-color: #E8EAF6;");
         descriptionCard.setStyle("-fx-effect: dropshadow(gaussian, silver, 10, 0, 0, 0); -fx-background-color: #E8EAF6; -fx-max-width: 200px;");
         VBox bidders = new VBox(new Label("BIDDERS: "), new Label(biddersName.toString()));
         bidders.setStyle("-fx-effect: dropshadow(gaussian, silver, 10, 0, 0, 0); -fx-background-color: #E8EAF6;");
-        contentPane.getChildren().addAll(imgPane,aboutCard,descriptionCard,bidders);
+        contentPane.getChildren().addAll(imgPane,aboutCard,bidPane,descriptionCard,bidders);
         return contentPane;
     }
 
